cfe5fa7b1e2407c43e18d12668fb0258773b045b
William Denton <wtd@pobox.com>
2016-12-20 11:22:32 -0500

    Fix bug; update docs

--- R/readable_marc245.R
+++ R/readable_marc245.R
@@ -1,7 +1,7 @@
 #' Readable MARC 245
 #'
 #' Convert raw MARC field into something human-readable
-#' @param title A MARC 245 title string (with subfields)
+#' @param title A MARC 245 title string (with subfields $a $b $c)
 #' @return A string
 #' @export
 #' @examples
@@ -11,11 +11,12 @@ readable_marc245 <- function (raw_245, include.author = TRUE) {
     ## Remove $a if any
     title <- gsub("\\$a ", "", raw_245)
     ## Collapse the subtitle
-    ## Drop the author and anything after
+    title <- gsub(": \\$b ", ": ", title)
+    ## Drop the author and anything after (if desired)
     if (include.author) {
-        title <- gsub(" : \\$c ", " / ", title)
+        title <- gsub(" / \\$c ", " / ", title)
     } else {
-        title <- gsub(" : \\$c.*", "", title)
+        title <- gsub(" / \\$c.*", "", title)
     }
     return(title)
 }
--- tests/testthat/test_245_titles.R
+++ tests/testthat/test_245_titles.R
@@ -1,6 +1,8 @@
 context("Check title readabilizing")
 
 test_that("Formatting is correct", {
-    expect_equal(readable_marc245("$a Title : $c Author"), "Title / Author")
-    expect_equal(readable_marc245("$a Title : $c Author", include.author = FALSE), "Title")
+    expect_equal(readable_marc245("$a R packages / $c Hadley Wickham"), "R packages / Hadley Wickham")
+    expect_equal(readable_marc245("$a R packages / $c Hadley Wickham", include.author = FALSE), "R packages")
+    expect_equal(readable_marc245("The order of things : $b an archaeology of the human sciences / $c Michel Foucault"), "The order of things : an archaeology of the human sciences / Michel Foucault")
+
 })
