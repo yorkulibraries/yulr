bc36c7fc1c6af9c849e9ec15fadf39e9ed8e49b8
William Denton <wtd@pobox.com>
2016-12-20 12:28:19 -0500

    Add a link_to_sp_books() test

--- tests/testthat/test_link_to_sp_books.R
+++ tests/testthat/test_link_to_sp_books.R
@@ -2,4 +2,5 @@ context("Check Scholars Portal Books links")
 
 test_that("VuFind link is correct", {
     expect_equal(link_to_sp_books("557306", "Marcus Aurelius: A Biography"), '<a href="https://books.scholarsportal.info/viewdoc.html?id=557306">Marcus Aurelius: A Biography</a>')
+    expect_equal(link_to_sp_books("/ebooks/ebooks3/upress/2013-05-05/1/9780773590915", "Ideological perspectives on Canada"), '<a href="https://books.scholarsportal.info/viewdoc.html?id=/ebooks/ebooks3/upress/2013-05-05/1/9780773590915">Ideological perspectives on Canada</a>')
 })
