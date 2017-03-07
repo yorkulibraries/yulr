d844394eac7deb4f3adf0cf0384b15c30122bd61
William Denton <wtd@pobox.com>
2017-03-07 14:49:52 -0500

    245 field: clean better

--- R/readable_marc245.R
+++ R/readable_marc245.R
@@ -2,6 +2,7 @@
 #'
 #' Convert raw MARC field into something human-readable
 #' @param title A MARC 245 title string (with subfields $a $b $c)
+#' @param include.author Include the $c subfield; default is TRUE
 #' @return A string
 #' @export
 #' @examples
@@ -11,12 +12,14 @@ readable_marc245 <- function (raw_245, include.author = TRUE) {
     ## Remove $a if any
     title <- gsub("\\$a ", "", raw_245)
     ## Collapse the subtitle
-    title <- gsub(": \\$b ", ": ", title)
+    title <- gsub(":\\s+\\$b\\s+", ": ", title)
     ## Drop the author and anything after (if desired)
     if (include.author) {
         title <- gsub(" / \\$c ", " / ", title)
     } else {
         title <- gsub(" / \\$c.*", "", title)
     }
+    ## Remove $h (format) if any
+    title <- gsub("\\s+\\$h\\s+", " ", title)
     return(title)
 }
--- tests/testthat/test_245_titles.R
+++ tests/testthat/test_245_titles.R
@@ -4,5 +4,7 @@ test_that("Formatting is correct", {
     expect_equal(readable_marc245("$a R packages / $c Hadley Wickham"), "R packages / Hadley Wickham")
     expect_equal(readable_marc245("$a R packages / $c Hadley Wickham", include.author = FALSE), "R packages")
     expect_equal(readable_marc245("The order of things : $b an archaeology of the human sciences / $c Michel Foucault"), "The order of things : an archaeology of the human sciences / Michel Foucault")
+    expect_equal(readable_marc245("Biocalculus :  $b  calculus, probability, and statistics for the life \  $c James Stewart and Troy Day", include.author = FALSE), "Biocalculus : calculus, probability, and statistics for the life \  $c James Stewart and Troy Day")
+    expect_equal(readable_marc245("Thoughts on an union $h [electronic resource] / $c by Joshua Spencer.", include.author = FALSE), "Thoughts on an union [electronic resource]")
 
 })
